44470c8b9e05c936ed3bb4238a7165838c25843f
TsAndriy <a.andriy.t@gmail.com>
2023-05-26 21:00:51 +0300

    Add navigation bar

--- header.php
+++ header.php
@@ -1,4 +1,34 @@
 <?php
-    include_once('inc/db.php');
-    include('inc/function.php');
+    include_once('include/db.php');
+    include('include/function.php');
 ?>
+<!doctype html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HuHouse</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">HuHouse</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <?php $menus=get_menu(); ?>
+                <?php foreach ($menus as $menu):?>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="category.php?category_id=<?=$menu['id']?>"><?=$menu['title'];?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+</nav>
--- include/function.php
+++ include/function.php
@@ -1,2 +1,10 @@
 <?php
 include('include/db.php');
+    function get_menu() {
+        global $conn;
+        $sql = "SELECT * FROM menu";
+
+        $result = mysqli_query($conn,$sql);
+        $menus = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $menus;
+    }
