e43cbf9d0542a6cfa54793331ddad87758e95df3
TsAndriy <a.andriy.t@gmail.com>
2023-05-27 12:21:10 +0300

    Add News

--- include/function.php
+++ include/function.php
@@ -8,3 +8,10 @@ include('include/db.php');
         $menus = mysqli_fetch_all($result, MYSQLI_ASSOC);
         return $menus;
     }
+    function get_news() {
+        global $conn;
+        $sql = "SELECT * FROM news";
+        $result = mysqli_query($conn, $sql);
+        $news = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $news;
+    }
